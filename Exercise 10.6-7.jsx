04c8ff87cd85f7d00831f63b5883f5c1b51bc9d2
Michael <Mick666@hotmail.com.au>
2020-12-27 12:54:33 +1100

    10.6-7

---
+++ Exercise 10.6-7.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, StyleSheet, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import { Link } from 'react-router-native';
+import Constants from 'expo-constants';
+
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: theme.backgroundColors.appBar,
+        paddingBottom: 25,
+        paddingLeft: 15,
+        flexDirection: 'row',
+    },
+    appBarText: {
+        fontWeight: theme.fontWeights.bold,
+        fontSize: theme.fontSizes.appBar,
+        color: theme.colors.textWhite,
+        paddingTop: 10,
+        paddingRight: 15,
+    }
+});
+
+const AppBar = () => {
+    return (
+        <View style={styles.container}>
+            <ScrollView horizontal>
+                <Link to='/login' component={TouchableWithoutFeedback}>
+                    <Text style={styles.appBarText}>Sign In</Text>
+                </Link>
+            </ScrollView>
+            <ScrollView horizontal>
+                <TouchableWithoutFeedback>
+                    <Text style={styles.appBarText}>Repositories</Text>
+                </TouchableWithoutFeedback>
+            </ScrollView>
+
+        </View>);
+};
+
+export default AppBar;
